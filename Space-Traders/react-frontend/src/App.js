0605237d2fb409ca0d0af8fea7d24a2c073c0a8a
cbiafore822 <cbiafore822@gmail.com>
2019-09-19 11:29:47 -0400

    Welcome page

--- Space-Traders/react-frontend/src/App.js
+++ Space-Traders/react-frontend/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import spaceship from './spaceship.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="Welcome">
+      <div id="stars">
+        <header id="Welcome-header">
+          <h1>Welcome to Space Traders!</h1>
+        </header>
+        <div>
+          <img src={spaceship} id="spaceship" align="left"/>
+        </div>
+        <button type="button" id="startButton">Start Game</button>
+        {/* <script type="text/javascript">
+          document.querySelector("startButton").addEventListner("click",
+            function() {
+              document.getElementById("spaceship").style.animation = "nextScreen 1 25s ease-in-out";
+            })
+        </script> */}
+      </div>
     </div>
   );
 }
