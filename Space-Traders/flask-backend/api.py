a26062a372d77a0999ee3a9861e88b338a9e1c19
Twinov <hoeltgemarc@gmail.com>
2019-09-30 18:52:47 -0400

    remove template code

--- Space-Traders/flask-backend/api.py
+++ Space-Traders/flask-backend/api.py
@@ -10,54 +10,6 @@ api = Api(app)
 
 space_traders = game.Game('easy', [1,1,1,1]) #TODO: make cleaner
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
-
-
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
-
-
-#parser = reqparse.RequestParser()
-#parser.add_argument('task')
-
-
-# Todo
-# shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
-    def get(self):
-        return TODOS
-
-    def post(self):
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
-
 class SpaceTraders(Resource):
     def get(self):
         print(space_traders)
@@ -84,8 +36,6 @@ class Travel(Resource):
 ##
 # Actually setup the Api resource routing here
 ##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
 
 api.add_resource(Travel, '/Space-Traders/travel/<planet_id>')
 api.add_resource(SpaceTraders, '/Space-Traders')
