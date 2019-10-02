5531d14a3866fb8931321ad5892e4a3b612b42e3
cbiafore822 <cbiafore822@gmail.com>
2019-10-02 19:31:37 -0400

    Update TravelMap.js

--- Space-Traders/react-frontend/src/TravelMap.js
+++ Space-Traders/react-frontend/src/TravelMap.js
@@ -11,13 +11,28 @@ function TravelMap() {
         <div className="circle">
         </div>
         <map name="planets">
-          <area shape="circle" coords="50vw,50vhs,100" alt="Sun" onClick={(e) => console.log("Hello")}/>
+          <area shape="circle" coords="50vw,50vhs,100" alt="Sun" onClick={(e) => getRegionData()}/>
           <area shape="circle" coords="90,58,3" alt="Mercury"/>
           <area shape="circle" coords="124,58,8" alt="Venus"/>
         </map>
       </div>
     </div>
   )
+
+  function getRegionData() {
+    console.log('updating')
+    var xhr = new XMLHttpRequest()
+  
+    // get a callback when the server responds
+    xhr.addEventListener('load', () => {
+      // update the state of the component with the result here
+      console.log(xhr.responseText)
+    })
+    // open the request with the verb and the url
+    xhr.open('GET', '/Space-Traders')
+    // send the request
+    xhr.send()
+  }
 }
 
 export default TravelMap
