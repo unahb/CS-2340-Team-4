82b1e027bf2b6519522b25a1e24fc1165dd5c813
cbiafore822 <cbiafore822@gmail.com>
2019-10-04 02:14:57 -0400

    added main parts of map UI

--- Space-Traders/react-frontend/src/TravelMap.js
+++ Space-Traders/react-frontend/src/TravelMap.js
@@ -3,18 +3,45 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 
 function TravelMap() {
-  const dummyREGIONS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   return (
-    <div id="Main">
+    <div id="mainMap">
       <div id="mapBack">
-        <img id="mapArea" usemap="#planets"/>
-        <div className="circle">
-        </div>
-        <map name="planets">
-          <area shape="circle" coords="50vw,50vhs,100" alt="Sun" onClick={(e) => getRegionData()}/>
-          <area shape="circle" coords="90,58,3" alt="Mercury"/>
-          <area shape="circle" coords="124,58,8" alt="Venus"/>
-        </map>
+        <button class="circle mercuryButt" onClick={(e) => {
+          regionSetup(-200, -200, e.target); // Replace 1st param with x-coord of planet and 2nd param with y-coord (from api data)
+        }}>Mercury</button>
+        <button class="circle venusButt" onClick={(e) => {
+          regionSetup(-200, 200, e.target);
+        }}>Venus</button>
+        <button class="circle earthButt" onClick={(e) => {
+          regionSetup(200, -200, e.target);
+        }}>Earth</button>
+        <button class="circle marsButt" onClick={(e) => {
+          regionSetup(200, 200, e.target);
+        }}>Mars</button>
+        <button class="circle jupiterButt" onClick={(e) => {
+          regionSetup(-150, 50, e.target);
+        }}>Jupiter</button>
+        <button class="circle saturnButt" onClick={(e) => {
+          regionSetup(0, -150, e.target);
+        }}>Saturn</button>
+        <button class="circle uranusButt" onClick={(e) => {
+          regionSetup(0, -100, e.target);
+        }}>Uranus</button>
+        <button class="circle neptuneButt" onClick={(e) => {
+          regionSetup(0, -50, e.target);
+        }}>Neptune</button>
+        <button class="circle plutoButt" onClick={(e) => {
+          regionSetup(0, 200, e.target);
+        }}>Pluto</button>
+        <button class="circle europaButt" onClick={(e) => {
+          regionSetup(50, -150, e.target);
+        }}>Europa</button>
+      </div>
+      <div id="planetInfo">
+        <label id="planetName">Name: </label>
+        <label id="planetTech">Technology: </label>
+        <label id="planetLoc">Location: </label>
+        <button type="button" id="travelTo">Travel</button>
       </div>
     </div>
   )
@@ -29,10 +56,20 @@ function TravelMap() {
       console.log(xhr.responseText)
     })
     // open the request with the verb and the url
-    xhr.open('GET', '/Space-Traders')
+    xhr.open('GET', '127.0.0.1:5000/Space-Traders')
     // send the request
     xhr.send()
   }
+}
+
+function regionSetup(x, y, butt) {
+  var radius = "2vw";
+  var left = new String(((x + 200) / 4.0) * 0.92) + "vw";
+  var top = new String((((y + 200) / 4.0) * 0.86)) + "vh";
+  butt.style.top = top;
+  butt.style.left = left;
+  butt.style.width = radius;
+  butt.style.height = radius;
 }
 
 export default TravelMap
