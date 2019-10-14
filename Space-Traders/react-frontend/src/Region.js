3a510857c1a13ae7360113408bd6651614f702f5
Tim Tan <timmytan18@gmail.com>
2019-10-14 15:20:08 -0400

    Make Region stateful

--- Space-Traders/react-frontend/src/Region.js
+++ Space-Traders/react-frontend/src/Region.js
@@ -2,35 +2,68 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import spaceship from './resources/spaceship.png';
 import './Region.css';
-import { put } from './requests';
+import { get, put } from './requests';
 
-function Region(props) {
+class Region extends React.Component {
+  constructor(props) {
+    super(props)
 
-  let region = props.location.region.name
-  let coordX = props.location.region.x_coordinate
-  let coordY = props.location.region.y_coordinate
+    this.state = {
+      player: {},
+      region: {},
+      ship: {},
+    }
+  }
 
-  let ship = props.location.ship
+  componentWillMount() {
+    console.log(this.props)
+    put(this.props.location.region.name, () => {
+      get((item) => {
+        this.setState({ player: item.Player, region: item.Player.region, ship: item.Ship })
+      })
+    })
+  }
 
-  put(region)
-  return (
-    <div id="Main">
-      <header id="Region-header">
-        <h1>{region.toUpperCase()}</h1>
-        <h1 style={{ fontSize: 20 }}>Location: ({coordX}, {coordY})</h1>
-      </header>
-      <Link
-        to={{
-          pathname: '/TravelMap',
-        }}
-        className="nav-link"
-      >
-        <button type="button" id="mapButton">
-          Map
-        </button>
-      </Link>
-    </div>
-  );
+  render() {
+    if (Object.entries(this.state.player).length !== 0) {
+      const region = this.state.region
+      const coordX = this.state.region.x_coordinate
+      const coordY = this.state.region.y_coordinate
+      const market = this.state.player.region.market
+      const ship = this.state.ship
+
+      // const table = market..map((row) =>
+      //   <tr>
+      //     <th>{row.name}</th>
+      //       <td>
+
+      //       </td>
+      //   </tr>
+      // )
+    
+      return (
+        <div id="Main">
+          <header id="Region-header">
+            <h1>{region.name.toUpperCase()}</h1>
+            <h1 style={{ fontSize: 20, marginTop: -15 }}>Location: ({coordX}, {coordY})</h1>
+          </header>
+
+          <Link
+            to={{
+              pathname: '/TravelMap',
+            }}
+            className="nav-link"
+          >
+            <button type="button" id="mapButton">
+              Map
+            </button>
+          </Link>
+        </div>
+      );
+    } else {
+      return (<div></div>)
+    }
+  }
 }
 
 export default Region;
