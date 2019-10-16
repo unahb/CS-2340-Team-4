87933b9315384fac3b91209207d48edb968c80f7
Tim Tan <timmytan18@gmail.com>
2019-10-16 10:13:38 -0400

    make table

--- Space-Traders/react-frontend/src/Region.js
+++ Space-Traders/react-frontend/src/Region.js
@@ -27,26 +27,36 @@ class Region extends React.Component {
   render() {
     if (Object.entries(this.state.player).length !== 0) {
       const region = this.state.region
-      const coordX = this.state.region.x_coordinate
-      const coordY = this.state.region.y_coordinate
       const market = this.state.player.region.market
       const ship = this.state.ship
 
-      // const table = market..map((row) =>
-      //   <tr>
-      //     <th>{row.name}</th>
-      //       <td>
+      let table = []
+      for (let [name, prices] of Object.entries(market)) {
+        console.log(prices)
+        const row =
+          <tr id="tableRow">
+            <th>{name}</th>
+            <td>{prices.Buy}</td>
+            <td>{prices.Sell}</td>
+          </tr>
+        table.push(row)
+      }
 
-      //       </td>
-      //   </tr>
-      // )
-    
       return (
         <div id="Main">
           <header id="Region-header">
             <h1>{region.name.toUpperCase()}</h1>
-            <h1 style={{ fontSize: 20, marginTop: -15 }}>Location: ({coordX}, {coordY})</h1>
+            <h1 style={{ fontSize: 20, marginTop: -15 }}>Location: ({region.x_coordinate}, {region.y_coordinate})</h1>
           </header>
+
+          <table id="table" align="left">
+            <tr>
+              <th>Items</th>
+              <th>Buy</th>
+              <th>Sell</th>
+            </tr>
+            {table}
+          </table>
 
           <Link
             to={{
